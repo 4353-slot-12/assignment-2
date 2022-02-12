143d328e834f9bcfae7da601b6057249a4b5e422
Adil Iqbal <aiqbal85@gmail.com>
2022-02-12 17:34:52 -0600

    navbar update

--- src/frontend/scripts/main.js
+++ src/frontend/scripts/main.js
@@ -1,2 +1,3 @@
-const authNavBar = document.getElementById('nav-authenticated-user');
-authNavBar.style.display = "none";
+// // Turn off auth navbar
+// const authNavBar = document.getElementById('nav-authenticated-user');
+// authNavBar.style.display = "none";
