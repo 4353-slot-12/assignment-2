d560ff6c8ed150a1ee97257d6c284a0ff899e7e2
Adil Iqbal <aiqbal85@gmail.com>
2022-02-12 16:59:02 -0600

    restore backend + history navbar update

---
+++ src/backend/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8000;
+const baseDir = path.join(process.cwd(), 'src');
+
+app.use(express.static(path.join(baseDir, 'frontend'), {
+    extensions: ['html', 'htm']
+}));
+
+app.listen(port, () => {
+  console.log(`team-12 assignment-2 listening on port ${port}`)
+});
